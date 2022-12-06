e4a91ee7c8de96c8de4619eeb006db07c830793e
Maruhermida <maruhermida08@gmail.com>
2022-12-05 23:14:13 -0300

    Sacar menu con opciones

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,3 +15,15 @@ if(navClose){
         navMenu.classList.remove('show-menu')
     })
 }
+
+/* Remove menu mobile */
+
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+
+    navMenu.classList.remove('show-menu')
+}
+
+navLink.forEach(n => n.addEventListener ('click', linkAction))
